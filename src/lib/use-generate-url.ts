dc82679333c621c984123b6c89d3b75565b2ae69
Enrique Santos <santosenrique869@gmail.com>
2023-11-11 20:24:48 -0300

    refactor: refactor useGenerateUrl to accept a generic schema

--- src/lib/use-generate-url.ts
+++ src/lib/use-generate-url.ts
@@ -1,41 +1,29 @@
 import { useCallback } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
-type SearchParam = {
-  params?: { [key: string]: string | null }[];
+type SearchParam<TSchema extends Record<string, string>> = {
+  params?: { [K in keyof TSchema]?: string | null };
 };
 
 const isEmpty = (value?: string | null) =>
-  value === null ||
-  value === "" ||
-  value?.trim().length === 0 ||
-  typeof value === "undefined";
+  value === null || value === "" || typeof value === "undefined";
 
-export function useGenerateUrl() {
+export function useGenerateUrl<TSchema extends Record<string, string>>() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const generateUrl = ({ params }: SearchParam) => {
+  const generateUrl = ({ params }: SearchParam<TSchema>) => {
     const newSearchParams = new URLSearchParams(searchParams);
 
-    params?.forEach((param) => {
-      const key = Object.keys(param)[0];
-      const value = param[key];
+    Object.entries(params ?? {}).forEach(([key, value]) => {
       const searchParamsHasKey = searchParams.has(key);
 
       if (searchParamsHasKey && isEmpty(value)) {
         newSearchParams.delete(key);
-        return;
-      }
-
-      if (searchParamsHasKey && !isEmpty(value)) {
+      } else if (searchParamsHasKey && !isEmpty(value)) {
         newSearchParams.set(key, value as string);
-        return;
-      }
-
-      if (!isEmpty(value)) {
+      } else if (!searchParamsHasKey && !isEmpty(value)) {
         newSearchParams.append(key, value as string);
-        return;
       }
     });
 
@@ -43,5 +31,5 @@ export function useGenerateUrl() {
     return `${pathname}?${newSearchParams.toString()}`;
   };
 
-  return useCallback(generateUrl, [searchParams, pathname]);
+  return useCallback(generateUrl, [pathname, searchParams]);
 }
